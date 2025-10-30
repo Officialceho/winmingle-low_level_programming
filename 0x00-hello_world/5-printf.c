43abffa9634281b175ca639c3e8215fec3e04c1f
officialceho <officialceho@gmail.com>
2025-10-30 20:32:30 +0000

    Add program that prints message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Okoye Chijioke Henry
+ * Program: WinMingle Community C Training
+ * Description: Prints a message using printf
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
