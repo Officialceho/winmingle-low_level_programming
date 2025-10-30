8d7465c8086b7677b78580ae9a5279a243f93f55
officialceho <officialceho@gmail.com>
2025-10-30 20:32:30 +0000

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Okoye Chijioke Henry
+ * Program: WinMingle Community C Training
+ * Description: Prints a message using puts
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
