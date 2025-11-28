23bc169a4e7278696e8c8d87c2759b4dabc85e39
officialceho <officialceho@gmail.com>
2025-11-28 10:51:43 +0000

    Add program that prints numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* Author: Okoye Chijioke Henry 
+* Program: WinMingle Community C Training
+* Description: Prints single-digit numbers of base 10 starting from 0.
+*/
+
+int main(void)
+  {
+      int n = 0;
+
+            while (n <= 9)
+                {
+                    printf("%d", n);
+                    n++;
+                }
+      printf("\n");
+      return 0;
+  }    
